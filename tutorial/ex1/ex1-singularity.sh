033b72500c60c7ebc3853447784ae97e07695b20
Soheil Soltani <8424923+soheil-soltani@users.noreply.github.com>
2020-11-16 11:12:01 +0100

    Create ex1-singularity.sh

---
+++ tutorial/ex1/ex1-singularity.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This example shows how to prepare a job script and use the container images to
+# run a training example with TensorFlow
+
+#SBATCH -A C3SE-STAFF
+#SBATCH -p alvis
+#SBATCH -t 00-00:05:00
+
+#SBATCH -n 1
+#SBATCH --gpus-per-node=V100:1
+
+singularity exec /apps/hpc-ai-containers/TensorFlow/TensorFlow_v2.1.0-tf2-py3-NGC-R20.03.sif python cnn_with_own_data_ex1.py > res_ex1_sing.out
