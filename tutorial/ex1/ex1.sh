4be5fc8050c5f7d5998b0b1f114b609ccda20cc4
Soheil <ssoheil@chalmers.se>
2020-11-16 10:40:42 +0100

    Correct the module load command

--- tutorial/ex1/ex1.sh
+++ tutorial/ex1/ex1.sh
@@ -11,7 +11,7 @@
 #SBATCH --gpus-per-node=V100:1
 
 
-ml GCC/8.3.0 CUDA/10.1.243 OpenMPI/3.1.4 TensorFlow/2.3.1-Python-3.7.4
+ml GCC/8.3.0 CUDA/10.1.243 OpenMPI/3.1.4 TensorFlow/2.1.0-Python-3.7.4
 ml Pillow/6.2.1
 
 python cnn_with_own_data_ex1.py > res_ex1.out
