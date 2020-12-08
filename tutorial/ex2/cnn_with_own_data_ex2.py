e493019661ba0f50c881faf86535403101b23488
Soheil <ssoheil@chalmers.se>
2020-12-08 13:26:31 +0100

    Update the main code

--- tutorial/ex2/cnn_with_own_data_ex2.py
+++ tutorial/ex2/cnn_with_own_data_ex2.py
@@ -3,9 +3,15 @@ from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Sequential
 
-img_path = '/cephyr/NOBACKUP/Datasets/YouTube-dataset/YouTubeVos_InstanceSegmentation_2019/train_all_frames/JPEGImages'
+
+#img_path = '/cephyr/NOBACKUP/Datasets/uni-freiburg-oVision-Scene-Flow-dataset/FlyingThings3D_subset/train/depth_boundaries'
+
+img_path = '/cephyr/NOBACKUP/Datasets/uni-freiburg-oVision-Scene-Flow-dataset/FlyingThings3D_subset/train/disparity_occlusions'
+
+
+
 train_batches = ImageDataGenerator().flow_from_directory(img_path, target_size=(10, 10), color_mode='grayscale',                                                         
-                                                         batch_size=13)
+                                                         batch_size=7273)
 
 #imgs, labels = next(train_batches)
 
@@ -13,9 +19,9 @@ train_batches = ImageDataGenerator().flow_from_directory(img_path, target_size=(
 # therefore, the input_shape is (xx, xx, 1), and the ImageDataGenerator should also 
 # be aware of that too: color_mode='grayscale'
 
-model = Sequential([layers.Conv2D(32, (3, 3), activation='relu', input_shape=(10, 10, 1)), 
+model = Sequential([layers.Conv2D(10, (3, 3), activation='relu', input_shape=(10, 10, 1)), 
                                  layers.Flatten(),
-                    layers.Dense(3471, activation='softmax'),
+                    layers.Dense(347, activation='softmax'),
                         
     
 ])
