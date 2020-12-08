a1b700d42950647cecf8059c4ae19cbc8eadf23a
Soheil <ssoheil@chalmers.se>
2020-12-08 13:10:59 +0100

    Add runscript for ex2 using singularity image

---
+++ tutorial/ex2/ex2_singularity.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This example shows how to use the available datasets to
+# train a CNN  with TensorFlow from an NGC container image. Investigate the cnn_with_own_data_ex2.py file to find 
+# out how to directly access the available datasets from your code.
+
+#SBATCH -A C3SE-STAFF
+#SBATCH -p alvis
+#SBATCH -t 01-00:00:00
+#SBATCH --gpus-per-node=V100:3
+
+
+
+singularity exec /apps/hpc-ai-containers/TensorFlow/TensorFlow_v2.1.0-tf2-py3-NGC-R20.03.sif python cnn_with_own_data_ex2.py > res_ex2.out
