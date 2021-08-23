04515f3a712ae7ce6536f7f44649eb1b44197361
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2021-08-23 11:26:41 +0200

    Update to correct container

--- tutorial/ex3/ex3_singularity.sh
+++ tutorial/ex3/ex3_singularity.sh
@@ -4,7 +4,7 @@
 #SBATCH -J pytorch_MNIST
 #SBATCH --gpus-per-node=T4:1
 
-CONTAINER=/apps/nvidia-ngc-containers/tensorflow/20.11-tf2-py3.sif
+CONTAINER=/apps/nvidia-ngc-containers/pytorch/20.11-py3.sif
 
 # Don't forget the --nv flag, else your containers won't see the GPUs!
 singularity exec --nv $CONTAINER python ./ex3_main.py > results_ex3.out
