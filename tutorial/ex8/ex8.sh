c2557d19441cda520970d2a54c22e66cc138f988
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2021-09-10 11:03:04 +0200

    Running CIFAR

--- tutorial/ex8/ex8.sh
+++ tutorial/ex8/ex8.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 #SBATCH -A C3SE-STAFF # find your project with the "projinfo" command
-#SBATCH --job-name="Alvis ex8"
-#SBATCH --time=0:10:0
+#SBATCH --job-name="CIFAR with Horovod"
+#SBATCH --time=1:00:0
 # For a multi-node job all GPUs on a node must be requested
 #SBATCH --gpus-per-node=T4:8
 # For Horovod the number of tasks-per-node must match the number of GPUs per node
@@ -11,17 +11,7 @@
 #SBATCH -e slurm-%j.err
 #SBATCH -o slurm-%j.out
 
-ml purge > /dev/null 2>&1
-module load GCC/10.1.0 \
-            CUDA/11.1.1 \
-            OpenMPI/4.0.5 \
-            Horovod/0.21.3-PyTorch-1.7.1
-
-# Download model if not available
-if [ ! -f pytorch_model.bin ]; then
-    wget https://huggingface.co/glasses/efficientnet_b2/blob/main/pytorch_model.bin
-fi
-#export OMPI_MCA_mpi_warn_on_fork=0
+module load GCC/10.2.0 CUDA/11.1.1 OpenMPI/4.0.5 Horovod/0.21.3-PyTorch-1.7.1 torchvision/0.8.2-PyTorch-1.7.1
 
 # Note the use of srun to start an instance of the program for each task
-srun python ex8.py
+srun python main.py --epochs=200
