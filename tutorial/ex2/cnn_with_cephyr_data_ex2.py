7ffc862cfb8fcf299cc8e9e46bac307d37d67e4d
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2021-09-20 18:52:23 +0200

    Chamera classification -> tiny-imagenet

--- tutorial/ex2/cnn_with_cephyr_data_ex2.py
+++ tutorial/ex2/cnn_with_cephyr_data_ex2.py
@@ -4,28 +4,23 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Sequential
 
 
-#img_path = '/cephyr/NOBACKUP/Datasets/uni-freiburg-oVision-Scene-Flow-dataset/FlyingThings3D_subset/train/depth_boundaries'
-
-img_path = '/cephyr/NOBACKUP/Datasets/uni-freiburg-oVision-Scene-Flow-dataset/FlyingThings3D_subset/train/disparity_occlusions'
+img_path = '/cephyr/NOBACKUP/Datasets/tiny-imagenet-200/train'
 
 
-
-train_batches = ImageDataGenerator().flow_from_directory(img_path, target_size=(10, 10), color_mode='grayscale',                                                         
+train_batches = ImageDataGenerator().flow_from_directory(img_path, target_size=(64, 64), color_mode="rgb",
                                                          batch_size=128)
-
-#imgs, labels = next(train_batches)
 
 # In defining the model, the input shape must match the dimension of the input data. We have grayscale images, 
 # therefore, the input_shape is (xx, xx, 1), and the ImageDataGenerator should also 
 # be aware of that too: color_mode='grayscale'
 
-model = Sequential([layers.Conv2D(10, (3, 3), activation='relu', input_shape=(10, 10, 1)), 
+model = Sequential([layers.Conv2D(10, (3, 3), activation='relu', input_shape=(64, 64, 3)), 
                     layers.Flatten(),
-                    layers.Dense(2, activation='softmax'),
+                    layers.Dense(200, activation='softmax'),
                     ])
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 model.summary()
 
-model.fit(train_batches, steps_per_epoch=267, epochs=10, verbose=2)
+model.fit(train_batches, steps_per_epoch=781, epochs=10, verbose=2)
--- tutorial/ex2/ex2.sh
+++ tutorial/ex2/ex2.sh
@@ -1,14 +1,15 @@
 #!/bin/env bash
 
 # This example shows how to use the available datasets to
-# train a CNN  with TensorFlow. Investigate the cnn_with_own_data_ex2.py file to find out how to directly access the available datasets from your code.
+# train a CNN  with TensorFlow. Investigate the cnn_with_cephyr_data_ex2.py
+# file to find out how to directly access the available datasets from your code.
 
 #SBATCH -A C3SE-STAFF  # find your project with the "projinfo" command
 #SBATCH -p alvis
-#SBATCH -t 01-00:00:00
-#SBATCH --gpus-per-node=T4:2
+#SBATCH -t 00-05:00:00
+#SBATCH --gpus-per-node=T4:1
 
-ml GCC/8.3.0 CUDA/10.1.243 OpenMPI/3.1.4 TensorFlow/2.3.1-Python-3.7.4 Pillow/6.2.1
+ml GCC/10.2.0  CUDA/11.1.1  OpenMPI/4.0.5 TensorFlow/2.5.0 Pillow/8.0.1
 
 python cnn_with_cephyr_data_ex2.py
 
