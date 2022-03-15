dd96f2ee509b8a1ca6b72dafe5c4577f8ac46737
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2022-03-15 16:23:29 +0100

    Add torchvision jupyter3.sh for later

--- tutorial/2_single_gpu_ml/jupyter3.sh
+++ tutorial/2_single_gpu_ml/jupyter3.sh
@@ -5,6 +5,7 @@ flat_modules
 
 ml purge
 ml PyTorch/1.8.1-fosscuda-2020b TensorFlow/2.5.0-fosscuda-2020b
+ml torchvision/0.9.1-fosscuda-2020b-PyTorch-1.8.1
 
 # You need to have a version of jupyter (e.g. JupyterLab or IPython  from the module system)
 ml JupyterLab/2.2.8-GCCcore-10.2.0 matplotlib/3.3.3-fosscuda-2020b Pillow/8.0.1-GCCcore-10.2.0
