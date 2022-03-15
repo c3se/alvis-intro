0133afdce6e636427602c88485af9d84d9d2d95e
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2022-03-15 16:38:18 +0100

    flat_modules -> ml purge

--- tutorial/3_loading_data/jobscript-pytorch.sh
+++ tutorial/3_loading_data/jobscript-pytorch.sh
@@ -3,11 +3,11 @@
 #SBATCH -A SNIC2021-7-120  # find your project with the "projinfo" command
 #SBATCH -p alvis
 #SBATCH -t 00:10:00
-#SBATCH --gpus-per-node=T4:1
+#SBATCH --gpus-per-node=A40:1
 #SBATCH -J "Data PyTorch"
 
 # Set-up environment
-flat_modules
+ml purge
 ml PyTorch/1.8.1-fosscuda-2020b torchvision/0.9.1-fosscuda-2020b-PyTorch-1.8.1 JupyterLab/2.2.8-GCCcore-10.2.0 matplotlib/3.3.3-fosscuda-2020b
 
 # Unpack data to TMPDIR
--- tutorial/3_loading_data/jobscript-tensorflow.sh
+++ tutorial/3_loading_data/jobscript-tensorflow.sh
@@ -3,11 +3,11 @@
 #SBATCH -A SNIC2021-7-120  # find your project with the "projinfo" command
 #SBATCH -p alvis
 #SBATCH -t 00:10:00
-#SBATCH --gpus-per-node=T4:1
+#SBATCH --gpus-per-node=A40:1
 #SBATCH -J "Data TensorFlow"
 
 # Set-up environment
-flat_modules
+ml purge
 ml TensorFlow/2.5.0-fosscuda-2020b matplotlib/3.3.3-fosscuda-2020b JupyterLab/2.2.8-GCCcore-10.2.0
 
 # Unpack data to TMPDIR
