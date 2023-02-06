8d37a4c406ff09ea1dd87dc4ef9fa037377ba585
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-02-06 10:04:19 +0100

    Specify tasks per node directly to srun

--- tutorial/7_multi_node_multi_gpu/TensorFlow/jobscript.sh
+++ tutorial/7_multi_node_multi_gpu/TensorFlow/jobscript.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
-#SBATCH -A SNIC2021-7-120  # find your project with the "projinfo" command
+#SBATCH -A SNIC2022-22-1064  # find your project with the "projinfo" command
 #SBATCH -p alvis
 #SBATCH -t 00:10:00
-#SBATCH --gpus-per-node=A100:4 --ntasks-per-node=4
+#SBATCH --gpus-per-node=A100:4
 #SBATCH -N 2
 #SBATCH -J "MNMG TensorFlow"  # Multi node, multiple GPUs
 
@@ -13,8 +13,8 @@ module purge
 ml TensorFlow/2.6.0-foss-2021a-CUDA-11.3.1
 
 # Run with MultiWorkerMirroredStrategy
-#srun python mwms.py --communicator=NCCL
-srun python mwms.py --communicator=RING
+srun -N $SLURM_NNODES --ntasks-per-node=$SLURM_GPUS_ON_NODE python mwms.py --communicator=NCCL
+#srun -N $SLURM_NNODES --ntasks-per-node=$SLURM_GPUS_ON_NODE python mwms.py --communicator=RING
 
 
 #=============================================================================
@@ -22,6 +22,6 @@ srun python mwms.py --communicator=RING
 #=============================================================================
 #module purge
 #ml Horovod/0.21.1-fosscuda-2020b-TensorFlow-2.4.1
-
-# Run with Horovod
-#srun python hvd.py
+#
+## Run with Horovod
+#srun -N $SLURM_NNODES --ntasks-per-node=$SLURM_GPUS_ON_NODE python hvd.py
