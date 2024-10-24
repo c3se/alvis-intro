00e690abc5f2ddc22db538970224d07dba7930f7
Sahar M. K. Al-Zaidawi <38066148+saharmahdie@users.noreply.github.com>
2024-10-24 17:15:40 +0200

    typo in alvis-intro-24.10.sh

--- tutorial/2_single_gpu_ml/alvis-intro-24.10.sh
+++ tutorial/2_single_gpu_ml/alvis-intro-24.10.sh
@@ -14,7 +14,7 @@ module purge
 
 module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
 module load TensorFlow/2.15.1-foss-2023a-CUDA-12.1.1
-module load matplotlib/matplotlib/3.7.2-gfbf-2023a
+module load matplotlib/3.7.2-gfbf-2023a
 
 # You need to have a version of jupyter (e.g. JupyterLab or IPython  from the module system)
 module load JupyterLab/4.0.5-GCCcore-12.3.0
