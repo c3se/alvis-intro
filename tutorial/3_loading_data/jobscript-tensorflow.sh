094d7fb5860c58e3fc1a6e87cce11ec4f92f45a3
Sahar M. K. Al-Zaidawi <38066148+saharmahdie@users.noreply.github.com>
2024-10-25 11:26:06 +0200

    Update modules in jobscript-tensorflow.sh

--- tutorial/3_loading_data/jobscript-tensorflow.sh
+++ tutorial/3_loading_data/jobscript-tensorflow.sh
@@ -8,9 +8,9 @@
 
 # Set-up environment
 ml purge
-ml TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
-ml matplotlib/3.5.2-foss-2022a
-ml JupyterLab/3.5.0-GCCcore-11.3.0
+ml TensorFlow/2.15.1-foss-2023a-CUDA-12.1.1
+ml matplotlib/3.7.2-gfbf-2023a
+ml JupyterLab/4.0.5-GCCcore-12.3.0
 
 # # Unpack data to TMPDIR
 # # uncomment if you want to try to read data from directory instead of archive
