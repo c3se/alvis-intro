bbb37fc010f769801f18601f9c97792c7dbadedf
Sahar M. K. Al-Zaidawi <38066148+saharmahdie@users.noreply.github.com>
2024-11-23 19:48:24 +0100

    remove JupyterLab from TensorBoard.sh

--- tutorial/5_profiling/TensorBoard.sh
+++ tutorial/5_profiling/TensorBoard.sh
@@ -15,6 +15,3 @@ module purge
 module load PyTorch-bundle/1.12.1-foss-2022a-CUDA-11.7.0
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 module load matplotlib/3.5.2-foss-2022a
-
-# You need to have a version of jupyter (e.g. JupyterLab or IPython  from the module system)
-module load JupyterLab/3.5.0-GCCcore-11.3.0
