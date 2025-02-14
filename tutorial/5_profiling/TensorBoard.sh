8dc3e0c6b3a14da3d49ec6afabb7132ff6462007
Sahar M. K. Al-Zaidawi <38066148+saharmahdie@users.noreply.github.com>
2025-02-14 11:46:01 +0100

    remove 5_profiling/TensorBoard.sh
    Co-authored-by: Viktor Rehnberg <35767167+VRehnberg@users.noreply.github.com>

--- tutorial/5_profiling/TensorBoard.sh
+++ tutorial/5_profiling/TensorBoard.sh
@@ -1,15 +1,3 @@
-#!/bin/env bash
-
-
-# This script is used for the Open Ondemand portal.
-# You can use it as a reference for creating a custom ~/portal/jupyter/my_jupyter_env.sh file
-
-#SBATCH -A NAISS2024-22-219    # find your project with the "projinfo" command
-#SBATCH -p alvis               # what partition to use (usually not necessary)
-#SBATCH -t 0-00:01:00          # how long time it will take to run
-#SBATCH --gpus-per-node=T4:1   # choosing no. GPUs and their type
-#SBATCH -J modules             # the jobname (not necessary)
-
 module purge
 
 module load PyTorch-bundle/1.12.1-foss-2022a-CUDA-11.7.0
